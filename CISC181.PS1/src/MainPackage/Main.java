4f8c1149f484ab9d712f8f0f4aa22c4fd794853f
Kimberly Wolff <Kimberly@udel.edu>
2016-02-10 16:24:29 -0500

    Added WolffK

--- CISC181.PS1/src/MainPackage/Main.java
+++ CISC181.PS1/src/MainPackage/Main.java
@@ -3,8 +3,13 @@ package MainPackage;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		/*
+		 * My Github Account Name: kwolff1895
+		 */
 			
-		GibbonsB.HelloWorld();
+		WolffK.HelloWorld();
+
 	}
 
 }
